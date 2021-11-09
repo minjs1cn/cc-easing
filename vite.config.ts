195ecf8dea1fd10046c14cc96901c244ba430a11
zhangmin.jammy <zhangmin.jammy@bytedance.com>
2021-11-09 16:09:47 +0800

    feat: tsc

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,4 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	build: {
-		lib: {
-			entry: 'src/main.ts',
-			formats: ['es'],
-			fileName: 'main',
-		},
-	},
-});
+export default defineConfig({});
